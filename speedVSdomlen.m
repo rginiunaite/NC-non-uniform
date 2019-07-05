be0b2e985b3f549fdb377bee5d1418b9241c91c2
rginiunaite <rasaginiu@gmail.com>
2019-07-05 11:42:43 +0100

    final dom len vs speed

---
+++ speedVSdomlen.m
@@ -0,0 +1,33 @@
+% cell hindered growth. I will plot cell speed versus total domain length
+
+
+
+speed = [1, 1.2, 1.3, 1.5, 2.0];
+% what do these correspond to
+speed = 0.7* speed;
+
+
+dom_length = [1116.39, 997.94, 925.84, 871.76, 864.033] % from domain length in cell-induced
+
+figure
+plot(speed,dom_length,'linewidth',4)
+
+
+
+set(gca,'FontSize',30)
+ax = gca;
+
+xlabel('Input cell speed, \mu m/min')
+ylabel('Final domain length, \mu m')
+
+ set(gca,'FontSize',30)
+ax = gca;
+
+
+ box on
+
+ set(gca,'linewidth',4)
+ 
+xticks([0.5 0.7,0.9,1.1,1.3,1.5,1.7])%,2.0])
+xticklabels({'0.5','0.7', '0.9','1.1','1.3','1.5','1.7'});%, '2.0'})
+xlim([0.6,1.5])
